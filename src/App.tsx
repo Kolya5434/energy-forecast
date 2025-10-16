b9ef5da5c4d09cbfe0061d26383ed43d27a11c78
Mykola <kovalenkokola69@gmail.com>
2025-10-16 19:59:55 +0300

    Update App.tsx includes Header and SidePanel

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,34 @@
-import './App.css';
 
 import { useState } from 'react';
 
-import { ThemeProvider } from '@mui/material';
+import { Box, CssBaseline, ThemeProvider } from '@mui/material';
 
 import { darkTheme, lightTheme } from '../theme';
 import { ApiProvider } from './context/ApiContext.tsx';
+import { Header } from './components/Header.tsx';
+import { SidePanel } from './components/SidePanel.tsx';
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(true);
+  const [isPanelOpen, setIsPanelOpen] = useState(true);
 
   const theme = isDarkMode ? darkTheme : lightTheme;
+  const togglePanel = () => {
+    setIsPanelOpen(!isPanelOpen);
+  };
   return (
     <ThemeProvider theme={theme}>
-      <ApiProvider>Energy forecast</ApiProvider>
+      <CssBaseline />
+      <ApiProvider>
+        <Box sx={{ display: 'flex', height: '100vh', bgcolor: 'background.default' }}>
+          <SidePanel isOpen={isPanelOpen} togglePanel={togglePanel} />
+          <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column', overflow: 'hidden' }}>
+            <Header toggleTheme={() => setIsDarkMode(!isDarkMode)} isPanelOpen={isPanelOpen} togglePanel={togglePanel} />
+            {/*<MainContent />*/}
+          </Box>
+          {/*<RightPanel />*/}
+        </Box>
+      </ApiProvider>
     </ThemeProvider>
   );
 }
