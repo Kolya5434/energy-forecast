1d2f0202800f416fe85e92cbea91877d3134e756
Mykola <kovalenkokola69@gmail.com>
2025-10-16 12:06:11 +0300

    Update response data for fetchEvaluation

--- src/api/fetchEvaluation.ts
+++ src/api/fetchEvaluation.ts
@@ -3,5 +3,9 @@ import type { IEvaluationApiResponse } from '../types/api.ts';
 
 export const fetchEvaluation = async (modelId: string): Promise<IEvaluationApiResponse> => {
   const response = await axiosInstance.get(`/api/evaluation/${modelId}`);
+  
+  response.data.accuracy_metrics.R2 = response.data.accuracy_metrics['R²'];
+  delete response.data.accuracy_metrics['R²'];
+  
   return response.data;
 };
