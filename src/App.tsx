9d3c721ba4a71132a74fecb8ddb281686c37d07b
Kolya5434 <5344668@stud.op.edu.ua>
2025-11-14 20:34:56 +0200

    Fix build

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense, useCallback, useMemo, useState } from 'react';
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/react';
 
 import { Box, CircularProgress, CssBaseline, ThemeProvider, useMediaQuery } from '@mui/material';
