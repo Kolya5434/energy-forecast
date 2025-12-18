67a025f4508dbd502efbee536fa9177ec40572bd
Kolya5434 <5344668@stud.op.edu.ua>
2025-12-18 17:38:33 +0200

    Upgrade dependencies and introduce `husky` and `lint-staged` for pre-commit setup.

---
+++ src/helpers/index.ts
@@ -0,0 +1,5 @@
+export { clampValue, FIELD_CONSTRAINTS } from './conditionsValidation';
+export { getHeatmapColor, isFeatureImportanceResponse } from './utils';
+export { exportEvaluationToXLSX, exportEvaluationToPDF_Quick, exportEvaluationToDOCX } from './exportEvaluationUtils';
+export { exportChartData } from './exportToFile';
+export { preloadXlsx, preloadJspdf, preloadDocx, preloadAllExportLibs } from './preloadExportLibs';
---
+++ src/hooks/index.ts
@@ -0,0 +1,18 @@
+export { useApiState, useCachedApiState } from './useApiState';
+export { useConditionsForm, INITIAL_CONDITIONS_FORM_STATE, type ConditionsFormState } from './useConditionsForm';
+export {
+  useUncertainty,
+  useEnsembleAnalysis,
+  useDriftStatus,
+  useDriftAnalysis,
+  useExplainability,
+  usePartialDependence,
+  useHorizonAnalysis,
+  useFeatureAnalysis,
+  useFeatureSelection,
+  useBenchmark,
+  useScalabilityTest,
+  useProbabilisticForecast,
+  useLiveMetrics,
+  useMetricsStream
+} from './useScientificV2';
